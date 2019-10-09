231b72d0dfe53976f32d79ac8afcb4244197768a
fritzvd <fritzvd+github@gmail.com>
2019-10-09 22:09:52 +0200

    new build

---
+++ precache-manifest.20cb15f068579432994a93a4b9bf4990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34996d2811d0dc27d4f3f5d3b74ea32",
+    "url": "/d6-rpg/index.html"
+  },
+  {
+    "revision": "1eed1c1a82a4ad226db4",
+    "url": "/d6-rpg/static/css/2.65d81660.chunk.css"
+  },
+  {
+    "revision": "786ed27e8f216fc4c178",
+    "url": "/d6-rpg/static/css/main.0de9bd07.chunk.css"
+  },
+  {
+    "revision": "1eed1c1a82a4ad226db4",
+    "url": "/d6-rpg/static/js/2.51735c5d.chunk.js"
+  },
+  {
+    "revision": "786ed27e8f216fc4c178",
+    "url": "/d6-rpg/static/js/main.d7423bb1.chunk.js"
+  },
+  {
+    "revision": "fac3320b62db053479e7",
+    "url": "/d6-rpg/static/js/runtime-main.c109c205.js"
+  }
+]);
